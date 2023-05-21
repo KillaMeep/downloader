a42dd2dd785ebd698fc73fa9516bcc0bd7bf1964
KillaMeep <45155241+KillaMeep@users.noreply.github.com>
2023-05-21 10:31:33 -0600

    Add files via upload

--- installer.bat
+++ installer.bat
@@ -3,12 +3,15 @@ mkdir Downloader
 cd Downloader
 curl --output yt-dlp.exe https://raw.githubusercontent.com/KillaMeep/downloader/main/yt-dlp.exe
 curl --output downloader.py https://raw.githubusercontent.com/KillaMeep/downloader/main/downloader.py
-mkdir ffmpeg
-cd ffmpeg
+mkdir ffmpeg_install
+cd ffmpeg_install
 curl --output ffmpeg.zip https://www.gyan.dev/ffmpeg/builds/packages/ffmpeg-5.1.2-essentials_build.zip
 tar -xf ffmpeg.zip
 del /s /q ffmpeg.zip
-ren ffmpeg* ffmpeg
+for /D %%i in (ffmpeg-*) do ren %%i ffmpeg
+move ffmpeg .. > nul
+cd ..
+del /s /q ffmpeg_install
 pip install tk
 pip install pyinstaller
 exit
