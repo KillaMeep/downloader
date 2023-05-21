9c4cd2117badaf4436b9f17505abd3d1df09ddcc
KillaMeep <45155241+KillaMeep@users.noreply.github.com>
2023-05-21 08:56:34 -0600

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,7 +1,7 @@
 @echo off
 mkdir Downloader
 cd Downloader
-curl --output yt-dlp.exe https://github.com/KillaMeep/downloader/raw/main/yt-dlp.exe
+curl --output yt-dlp.exe https://raw.githubusercontent.com/KillaMeep/downloader/main/yt-dlp.exe
 curl --output downloader.py https://raw.githubusercontent.com/KillaMeep/downloader/main/downloader.py
 curl --output build.bat https://raw.githubusercontent.com/KillaMeep/downloader/main/build.bat
 pip install tk
