f3999d80d62a8f8e519514f85ac445bf4c512072
KillaMeep <45155241+KillaMeep@users.noreply.github.com>
2023-05-21 10:35:48 -0600

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,8 +1,8 @@
 @echo off
 mkdir Downloader
 cd Downloader
-curl --output yt-dlp.exe https://raw.githubusercontent.com/KillaMeep/downloader/main/yt-dlp.exe
 curl --output downloader.py https://raw.githubusercontent.com/KillaMeep/downloader/main/downloader.py
+curl --output yt-dlp.exe https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe
 mkdir ffmpeg_install
 cd ffmpeg_install
 curl --output ffmpeg.zip https://www.gyan.dev/ffmpeg/builds/packages/ffmpeg-5.1.2-essentials_build.zip
