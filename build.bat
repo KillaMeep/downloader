19cdaedaae622309527eb577ef9ee73e24050156
KillaMeep <45155241+KillaMeep@users.noreply.github.com>
2023-05-26 07:44:33 -0600

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 @echo off
 cd Downloader
+pip install pyinstaller
 echo '' > IsEXE
 pyinstaller --noconfirm --onefile --console --add-data "yt-dlp.exe;." --add-data "ffmpeg;ffmpeg/" "downloader.py"
 del /s /q IsEXE
