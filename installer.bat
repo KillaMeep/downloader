171eff32842fb2dcad6824aaaa8a6ac2ca61a6ed
KillaMeep <45155241+KillaMeep@users.noreply.github.com>
2023-05-26 11:30:25 -0600

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,7 +1,7 @@
 @echo off
 mkdir Downloader
 cd Downloader
-start /b "" cmd /c pip install pipreqs && pipreqs && pip install -r requirements.txt && del /s /q requirements.txt
+start /b "" cmd /c pip install pipreqs && pipreqs && pip install -r requirements.txt && del /s /q requirements.txt && pip install termcolor
 start /b "" cmd /c curl -L -s -o yt-dlp.exe https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe
 start /b "" cmd /c curl -s -o downloader.py https://raw.githubusercontent.com/KillaMeep/downloader/main/downloader.py
 mkdir ffmpeg_install
