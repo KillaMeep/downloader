8e948e000be21bd3b4511b34a314cbc28706cb9d
KillaMeep <45155241+KillaMeep@users.noreply.github.com>
2023-05-26 07:41:21 -0600

    multithreaded downloading
    MUUUCH faster for installing.

--- installer.bat
+++ installer.bat
@@ -1,15 +1,12 @@
 @echo off
 mkdir Downloader
 cd Downloader
-pip install pipreqs
-pipreqs
-pip insall -r requirements.txt
-del /s /q requirements.txt
-curl --output downloader.py https://raw.githubusercontent.com/KillaMeep/downloader/main/downloader.py
-curl -L -O https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe
+start /b "" cmd /c pip install pipreqs && pipreqs && pip install -r requirements.txt && del /s /q requirements.txt
+start /b "" cmd /c curl -L -s -o yt-dlp.exe https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe
+start /b "" cmd /c curl -s -o downloader.py https://raw.githubusercontent.com/KillaMeep/downloader/main/downloader.py
 mkdir ffmpeg_install
 cd ffmpeg_install
-curl --output ffmpeg.zip https://www.gyan.dev/ffmpeg/builds/packages/ffmpeg-5.1.2-essentials_build.zip
+curl -s -o ffmpeg.zip https://www.gyan.dev/ffmpeg/builds/packages/ffmpeg-5.1.2-essentials_build.zip
 tar -xf ffmpeg.zip
 del /s /q ffmpeg.zip
 for /D %%i in (ffmpeg-*) do ren %%i ffmpeg
