4711c48fbaf7723f627264850cd08edd95319edb
KillaMeep <45155241+KillaMeep@users.noreply.github.com>
2023-05-26 07:25:54 -0600

    Add files via upload

--- build.bat
+++ build.bat
@@ -5,6 +5,7 @@ pyinstaller --noconfirm --onefile --console --add-data "yt-dlp.exe;." --add-data
 del /s /q IsEXE
 del /s /q downloader.spec
 rmdir /s /q build
-copy dist/downloader.exe ../..
+copy dist/downloader.exe ..
 rmdir /s /q dist
-exit
+copy downloader.exe ..
+
