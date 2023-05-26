34f588ad4885cfb9fc7251b4e286b290eff727e8
KillaMeep <45155241+KillaMeep@users.noreply.github.com>
2023-05-26 10:05:22 -0600

    Update build.bat

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 cd Downloader
 pip install pyinstaller
 echo '' > IsEXE
-pyinstaller --noconfirm --onefile --console --add-data "yt-dlp.exe;." --add-data "ffmpeg;ffmpeg/" "downloader.py"
+pyinstaller --noconfirm --onefile --console --add-data "yt-dlp.exe;." --add-data "ffmpeg;ffmpeg/" --add-data "IsEXE;."  "downloader.py"
 del /s /q IsEXE
 del /s /q downloader.spec
 rmdir /s /q build
