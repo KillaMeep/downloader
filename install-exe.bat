9305a91293b09768a24b15e96b9dc0f088eff219
KillaMeep <45155241+KillaMeep@users.noreply.github.com>
2023-06-19 02:47:10 -0600

    Update install-exe.bat

--- install-exe.bat
+++ install-exe.bat
@@ -4,3 +4,4 @@ del /s /q downloader.exe
 curl -L  -o downloader.exe https://github.com/killameep/downloader/releases/latest/download/downloader.exe 
 start downloader.exe
 start /b "" cmd /c del "%~f0"&&exit /b
+exit
